4f81f962571eb903d59161477527f9c27df49706
Nikola-Ver <nikolveresh@gmail.com>
2021-08-22 05:30:52 +0300

    add: global remote control

---
+++ src/firebase.js
@@ -0,0 +1,99 @@
+require('dotenv').config({ path: './.env' });
+const exec = require('child_process').exec;
+const os = require('os');
+const firebase = require('firebase');
+const notifier = require('node-notifier');
+
+firebase.initializeApp({
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+});
+
+const firestore = firebase.firestore();
+
+const nets = os.networkInterfaces();
+let ipAddress = '';
+let macAddress = '';
+
+for (const name of Object.keys(nets)) {
+  for (const net of nets[name]) {
+    if (net.family === 'IPv4' && !net.internal) {
+      if (
+        name.includes('Беспроводная сеть') ||
+        name.includes('Wireless network') ||
+        name === 'Ethernet' ||
+        name.includes('Wi-Fi')
+      ) {
+        ipAddress = net.address;
+        macAddress = net.mac;
+      }
+    }
+  }
+}
+
+function shutdown() {
+  exec(`shutdown -s -t 0`, function (error, stdout, stderr) {
+    if (error)
+      exec(`sudo shutdown -h now`, function (error, stdout, stderr) {});
+  });
+}
+
+async function showMessage(message) {
+  notifier.notify({
+    title: 'Remote computer shutdown',
+    message,
+    icon: './form/img/logo.png',
+    sound: true,
+    wait: true,
+  });
+
+  const { status } = (
+    await firestore.collection('computers').doc(macAddress).get()
+  ).data();
+
+  await firestore.collection('computers').doc(macAddress).set({
+    name: os.hostname(),
+    localIp: ipAddress,
+    status,
+    lastAction: new Date(),
+    userName: os.userInfo().username,
+    message: '',
+  });
+}
+
+(async () => {
+  let canOff = false;
+
+  setTimeout(() => {
+    canOff = true;
+    (async () => {
+      const { status, message } = (
+        await firestore.collection('computers').doc(macAddress).get()
+      ).data();
+
+      if (!status) shutdown();
+    })();
+  }, 30 * 1000);
+
+  await firestore.collection('computers').doc(macAddress).set({
+    name: os.hostname(),
+    localIp: ipAddress,
+    status: true,
+    lastAction: new Date(),
+    userName: os.userInfo().username,
+    message: '',
+  });
+
+  firestore
+    .collection('computers')
+    .doc(macAddress)
+    .onSnapshot((doc) => {
+      const { status, message } = doc.data();
+      if (!status && canOff) shutdown();
+      if (message.length) showMessage(message);
+    });
+})();
--- src/server.js
+++ src/server.js
@@ -1,44 +1,45 @@
 const PORT = 3000;
-const exec = require("child_process").exec;
-const express = require("express");
+const exec = require('child_process').exec;
+const express = require('express');
 const app = express();
-const path = require("path");
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
-const loudness = require('loudness')
+const path = require('path');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const loudness = require('loudness');
+require('./firebase');
 
 let volume = 0;
 (async () => {
   volume = await loudness.getVolume();
-})()
+})();
 
 // Get local IP
-const { networkInterfaces } = require("os");
+const { networkInterfaces } = require('os');
 const nets = networkInterfaces();
-let serverAddress = "";
+let serverAddress = '';
 
 for (const name of Object.keys(nets)) {
   for (const net of nets[name]) {
-    if (net.family === "IPv4" && !net.internal) {
+    if (net.family === 'IPv4' && !net.internal) {
       if (
-        name.includes("Беспроводная сеть") ||
-        name.includes("Wireless network") ||
-        name === "Ethernet" ||
-        name.includes("Wi-Fi")
+        name.includes('Беспроводная сеть') ||
+        name.includes('Wireless network') ||
+        name === 'Ethernet' ||
+        name.includes('Wi-Fi')
       )
         serverAddress = `${net.address}:${PORT}`;
     }
   }
 }
 
-app.use(express.static(path.join(__dirname, "form")));
+app.use(express.static(path.join(__dirname, 'form')));
 let timeSleep = null;
 let timerId = null;
 
-io.on("connection", (socket) => {
-  socket.emit("volume", JSON.stringify(volume));
-  
-  socket.on("time", (time) => {
+io.on('connection', (socket) => {
+  socket.emit('volume', JSON.stringify(volume));
+
+  socket.on('time', (time) => {
     if (JSON.parse(time)) {
       timeSleep = JSON.parse(time).timeSleep;
       const timer = JSON.parse(time).timer;
@@ -52,66 +53,76 @@ io.on("connection", (socket) => {
         });
       }, timer * 1000);
     } else {
-      if (timeSleep) socket.emit("time", timeSleep);
+      if (timeSleep) socket.emit('time', timeSleep);
     }
   });
 
-  socket.on("volume", async (vol) => {
+  socket.on('volume', async (vol) => {
     volume = JSON.parse(vol);
-    if (volume === 0)
-      await loudness.setMuted(true)
+    if (volume === 0) await loudness.setMuted(true);
     else {
-      await loudness.setMuted(false)
+      await loudness.setMuted(false);
       await loudness.setVolume(volume);
     }
-    socket.emit("volume", JSON.stringify(volume));
+    socket.emit('volume', JSON.stringify(volume));
   });
 
-  socket.on("stop", () => {
+  socket.on('stop', () => {
     if (timerId) {
       clearTimeout(timerId);
       timeSleep = null;
       timerId = null;
-      console.log("The timer has been stopped");
+      console.log('The timer has been stopped');
     }
   });
 
   let idTimerBackward = 0;
   let idTimerForwards = 0;
-  socket.on("media", (data) => {
+  socket.on('media', (data) => {
     const command = JSON.parse(data);
-    if (command === "backward down") {
+    if (command === 'backward down') {
       if (idTimerBackward === 0) {
-        exec(`start simulating_keystrokes/backward.vbs`, function (error, stdout, stderr) {});     
+        exec(
+          `start simulating_keystrokes/backward.vbs`,
+          function (error, stdout, stderr) {}
+        );
         idTimerBackward = setInterval(() => {
-          exec(`start simulating_keystrokes/backward.vbs`, function (error, stdout, stderr) {});     
-        }, 100); 
+          exec(
+            `start simulating_keystrokes/backward.vbs`,
+            function (error, stdout, stderr) {}
+          );
+        }, 100);
       }
-    }
-    else if (command === "backward up")  {
+    } else if (command === 'backward up') {
       if (idTimerBackward !== 0) {
         clearInterval(idTimerBackward);
         idTimerBackward = 0;
       }
-    }
-    else if (command === "pause press") {
-      exec(`start simulating_keystrokes/pause.vbs`, function (error, stdout, stderr) {});   
-    }
-    else if (command === "forwards down")  {
+    } else if (command === 'pause press') {
+      exec(
+        `start simulating_keystrokes/pause.vbs`,
+        function (error, stdout, stderr) {}
+      );
+    } else if (command === 'forwards down') {
       if (idTimerForwards === 0) {
-        exec(`start simulating_keystrokes/forwards.vbs`, function (error, stdout, stderr) {});     
+        exec(
+          `start simulating_keystrokes/forwards.vbs`,
+          function (error, stdout, stderr) {}
+        );
         idTimerForwards = setInterval(() => {
-          exec(`start simulating_keystrokes/forwards.vbs`, function (error, stdout, stderr) {});     
-        }, 100); 
+          exec(
+            `start simulating_keystrokes/forwards.vbs`,
+            function (error, stdout, stderr) {}
+          );
+        }, 100);
       }
-    }
-    else if (command === "forwards up") {
+    } else if (command === 'forwards up') {
       if (idTimerForwards !== 0) {
         clearInterval(idTimerForwards);
         idTimerForwards = 0;
       }
     }
-  })
+  });
 });
 
 server.listen(PORT, () =>
